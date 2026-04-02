851eeb7e79ea359f3daa8423a0a5e2641aa77d41
Luteslino <lucas.pf18@aluno.ifsc.edu.br>
2026-04-02 16:33:58 -0300

    Evolui a classe Aluno com notas e cálculo de média

--- roteiro02/main.py
+++ roteiro02/main.py
@@ -6,8 +6,25 @@ alunos = [
     Aluno("Carlos", 18)
 ]
 
+alunos[0].adicionar_nota(6)
+alunos[0].adicionar_nota(7)
+alunos[0].adicionar_nota(8)
+
+alunos[1].adicionar_nota(5)
+alunos[1].adicionar_nota(6)
+alunos[1].adicionar_nota(7)
+
+alunos[2].adicionar_nota(8)
+alunos[2].adicionar_nota(9)
+alunos[2].adicionar_nota(10)
+
 for aluno in alunos:
     print(aluno.apresentar())
+    print("Média:", aluno.calcular_media())
 
-    if aluno.maior_de_idade():
-        print(f"{aluno.nome} é maior de idade.")
+    if aluno.aprovado():
+        print("Aprovado")
+    else:
+        print("Reprovado")
+        
+    print("-" * 20)
